dce0a21953a8839a7f91c05edf2871ac1a9be844
Michael Mateo <michaelallen.mateo@gmail.com>
2018-09-27 04:11:34 +0800

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+
+//complie Sass
+
+gulp.task("sass", function () {
+    return gulp.src(["./app/sass/*.scss"]).pipe(sass()).pipe(autoprefixer({browsers: ["last 2 versions"], cascade: false})).on("error", function (errorInfo) {
+        console.log(errorInfo.toString());
+        this.emit("end");
+    }).pipe(gulp.dest("./app/css")).pipe(browserSync.stream());
+});
+
+//watch & serve
+
+gulp.task("watch", ["sass"], function () {
+    browserSync.init({server: "./app", notify: false});
+
+    gulp.watch(["./app/sass/*.scss"], ["sass"]);
+    gulp.watch(["./app/*.html"]).on("change", browserSync.reload);
+});
+
+//default
+// gulp.task("default", ["serve"]);
---
+++ readme.txt
@@ -0,0 +1,36 @@
+A package for compiling SASS and running a dev server
+
+Installation:
+
+     - To install dependecies (in package.json)
+
+ $ npm install
+
+Run
+    - To Run the server and watch changes in SASS file
+ 
+ $ gulp watch
+
+how to use this workflow:
+
+1. copy package.json and gulpfile.js
+
+2.  in your gitbash or terminal type: npm install
+
+3.  in your gitbash or terminal type:  gulp watch
+
+
+NOTES:
+
+Folder Structure
+
+app
+    -css
+        -main.css
+    -img
+    -sass
+        -main.scss
+    -index.html
+nodes_module
+gulpfile.js
+package.json            
